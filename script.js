11b3565c18626de431cbf9afb29f1f1180db5579
papaindiansierra <muazzam0107@gmail.com>
2025-12-07 12:11:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 function toggleDetail(id) {
     const detail = document.getElementById(`detail-${id}`);
-    if (detail.style.display === "block") {
-        detail.style.display = "none";
-    } else {
-        detail.style.display = "block";
-    }
+    detail.style.display =
+        detail.style.display === "block" ? "none" : "block";
 }
